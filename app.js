c14ec1dd50d16098b15ff433f78a2c26658e63ae
Sean Wong <wotzhs@gmail.com>
2017-07-02 21:59:19 +0800

    scaffold basic files

---
+++ app.js
@@ -0,0 +1,20 @@
+const express    = require('express');
+const bodyParser = require('body-parser');
+const routes     = require('./routes/routes');
+const app        = express();
+
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/disconeq');
+
+app.use(( req, res, next ) => {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-access-token");
+	next();
+} )
+app.use(bodyParser.json());
+
+routes(app);
+
+module.exports = app
