4c65ed45f9f99d1cedc38c2fddd1b8d6de00fa7f
Sean Wong <wotzhs@gmail.com>
2017-07-06 00:30:57 +0800

    set header allowed methods

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ mongoose.connect('mongodb://localhost/disconeq');
 app.use(( req, res, next ) => {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-access-token");
-	res.header("Access-Control-Allow-Methods", "GET, POST, PATCH");
+	res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
 	next();
 } )
 app.use(bodyParser.json());
