d22a51ad6c8fab72fa48a97478a771ea4cb47e64
Sean Wong <wotzhs@gmail.com>
2017-07-05 22:52:54 +0800

    add header allow methods to include patch

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ mongoose.connect('mongodb://localhost/disconeq');
 app.use(( req, res, next ) => {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-access-token");
+	res.header("Access-Control-Allow-Methods", "GET, POST, PATCH");
 	next();
 } )
 app.use(bodyParser.json());
